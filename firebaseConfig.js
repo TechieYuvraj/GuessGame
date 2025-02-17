fcb3579ca4ee42a222ba86825b3fb2a2313f2c31
Yuvraj Singh <106911886+TechieYuvraj@users.noreply.github.com>
2025-02-17 21:30:23 +0530

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA8bHJ-Egs4OYhn-WoBzZ2mE_9OhUS9_Aw",
+    authDomain: "guess-game-47c4d.firebaseapp.com",
+    databaseURL: "https://guess-game-47c4d-default-rtdb.firebaseio.com",
+    projectId: "guess-game-47c4d",
+    storageBucket: "guess-game-47c4d.firebasestorage.app",
+    messagingSenderId: "731139434958",
+    appId: "1:731139434958:web:94884756283691d8f5e39f",
+    measurementId: "G-9LDC3K8PNR"
+};
+
+export default firebaseConfig;
